4760e6aa88631fcd00dc04e9962de1337cc9e9b7
Ol_Kuts <kuts.ol.vas@gmail.com>
2022-06-09 05:58:28 +0300

    add cart

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import {nav} from "../config/data";
+import { FaCartArrowDown } from 'react-icons/fa';
 
 export const Nav = () => (
     <nav>
+        <FaCartArrowDown className={'cart'}/>
         {nav.map(item => <div key={item}>{item}</div>)}
     </nav>
 
